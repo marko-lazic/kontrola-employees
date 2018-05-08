7aa3a9b162e21c7a22d28d0b27b42b7a4071bb4c
Marko Lazic <zila015@gmail.com>
2018-05-08 13:21:07 +0200

    Initial commit

---
+++ src/main/java/ml/mlazic/kontrolaemployees/KontrolaEmployeesApplication.java
@@ -0,0 +1,12 @@
+package ml.mlazic.kontrolaemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontrolaEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontrolaEmployeesApplication.class, args);
+	}
+}
